450fff163288e361c7bae8b9dea2f27924dc9ebe
Dion Laranjeira <dionribeiro.rr@gmail.com>
2021-12-29 11:33:26 -0400

    Criação da interface do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 
-
 void main(){
-
-  runApp( MaterialApp(
-    title: "Frases do dia",
-    home: Container(color: Colors.white,),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Home(),
   ));
+}
 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        // decoration: BoxDecoration(
+        //   border: Border.all(width: 3, color: Colors.amber)
+        // ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Text("Clique abaixo para gerar uma frase",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+            ),
+            ElevatedButton(
+                onPressed: (){},
+                child: Text("NOVA FRASE"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.lightGreen
+                ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
