bbdcc1bcd46b2c8529b0aeb27f60e22cda725bf8
Dion Laranjeira <dionribeiro.rr@gmail.com>
2021-12-29 12:10:51 -0400

    Função gerarFrase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main(){
   runApp(MaterialApp(
@@ -15,6 +16,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var _frases = [
+    "As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.",
+    "Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.",
+    "Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.",
+    "No meio da dificuldade encontra-se a oportunidade.",
+    "Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.",
+    "Eu faço da dificuldade a minha motivação. A volta por cima vem na continuação.",
+    "A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.",
+    "Pedras no caminho? Eu guardo todas. Um dia vou construir um castelo.",
+    "É parte da cura o desejo de ser curado.",
+    "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado."
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase";
+  
+  void _gerarFrase(){
+    var numeroSorteado = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +57,7 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Image.asset("images/logo.png"),
-            Text("Clique abaixo para gerar uma frase",
+            Text(_fraseGerada,
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 20,
@@ -42,7 +66,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             ElevatedButton(
-                onPressed: (){},
+                onPressed: _gerarFrase,
                 child: Text("NOVA FRASE"),
                 style: ElevatedButton.styleFrom(
                   primary: Colors.lightGreen
